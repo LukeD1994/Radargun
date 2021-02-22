ae794ecd43df286e5f5e3ad2fe48f619d6b6fa86
Luke <luke.dust@hotmail.co.uk>
2021-02-22 10:39:10 +0000

    Updated resource
    Added command check and ability to disable keybind check for servers with lots of plugins/scripts fighting for GTA keys

--- __resource.lua
+++ __resource.lua
@@ -1,5 +1,10 @@
 resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
+author 'Brock (Edited by LukeD)'
+description 'Remastered edition of Brock\'s radargun script, edited by LukeD for RPHPD'
+version '1.0.0'
+
+
 ui_page 'html/index.html'
 
 files {
--- client.lua
+++ client.lua
@@ -25,7 +25,38 @@ Citizen.CreateThread(function()
 	end
 end)
 
+--LukeD Edits to grant command usage as well
+local RGEnabled = false;
+RegisterCommand('rg', function(source, args, rawCommand)
+	if getSelectedWeapon then
+		if RGEnabled == true then
+			RGEnabled = false
+			SendNUIMessage({
+				action = "close",
+			})
+		else
+			RGEnabled = true
+			SendNUIMessage({
+				action = "open",
+			})
+		end
+	else
+		TriggerEvent('chat:addMessage', {
+			color = { 255, 0, 0},
+			multiline = true,
+			args = {"Error", "You need to equip the radargun to use this command! (Equip: " .. cfg.radargun .. ")"}
+		})
+
+
+	end
+end, false)
+
+TriggerEvent('chat:addSuggestion', '/rg', 'Toggles the hand held radar gun menu open and closed.', {})
+
+
+
 Citizen.CreateThread( function()
+	
 	while true do
 		Citizen.Wait(1)
 
@@ -39,31 +70,35 @@ Citizen.CreateThread( function()
 		end
 		-- ////////////////////////////////////////////////////////////////
 
+		if cfg.disablekey == false then
+			if IsControlJustPressed(1, cfg.menuopen)then --246 = Y
 
-		if IsControlJustPressed(1, cfg.menuopen) then --246 = Y
-
-			if getSelectedWeapon then
-				if shown == true then
-					shown = false
-					SendNUIMessage({
-						action = "close",
-					})
+				if getSelectedWeapon then
+					if shown == true then
+						shown = false
+						RGEnabled = false
+						SendNUIMessage({
+							action = "close",
+						})
+					else
+						SendNUIMessage({
+							action = "open",
+						})
+						shown = true
+						RGEnabled = true
+					end
 				else
-					SendNUIMessage({
-						action = "open",
-					})
-					shown = true
-				end
-			else
-				if shown == true then
-					SendNUIMessage({
-						action = "close",
-					})
-					shown = false
+					if shown == true then
+						SendNUIMessage({
+							action = "close",
+						})
+						shown = false
+						RGEnabled = false
+					end
 				end
 			end
 		end
-
+		
 		if shown then
 			if getSelectedWeapon then
 				if IsControlJustPressed(1, cfg.bottomfreeze) then --38 = E
